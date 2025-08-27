f8a6b7eed4bf074f8f0178d931a357d11ab3ccb6
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-08-27 21:40:31 +0300

    Create part0.sh

---
+++ script/lg500/part0.sh
@@ -0,0 +1,29 @@
+#ArchWay Lenovo G500 part0
+mkfs.vfat /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+pacstrap /mnt base linux linux-firmware coreutils iproute2 dhcpcd iwd grub sudo nano fastfetch util-linux
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt <<EOF
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+read -p "Enter your timezone (example Europe/Moscow): " timezone
+ln -sf /usr/share/zoneinfo/$timezone /etc/localtime # Adjust to your timezone
+hwclock --systohc --utc
+echo ArchWayBTW > /etc/hostname
+passwd
+read -p "Enter desired username: " username
+useradd -m -s /bin/bash $username
+passwd $username
+EDITOR=nano visudo # Manual editing
+systemctl enable dhcpcd.service
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+EOF
+echo "ArchWay Linux Installed! reboot in 5 seconds"
+umount -R /mnt
+sleep 5
+reboot
