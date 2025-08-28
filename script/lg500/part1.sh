7eae6e79f95ff284a60e35826e131566c2785ae1
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-08-28 08:58:27 +0300

    Update part1.sh

--- script/lg500/part1.sh
+++ script/lg500/part1.sh
@@ -1,3 +1,8 @@
+echo "=================================================================="
+echo "                 ArchWay Part1.sh "
+echo "=================================================================="
+
+arch-chroot /mnt <<EOF
 read -p "Enter your timezone (example Europe/Moscow): " timezone
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime # Adjust to your timezone
 hwclock --systohc --utc
@@ -9,3 +14,9 @@ passwd
 read -p "Enter desired username: " username
 useradd -m -s /bin/bash $username
 passwd $username
+exit
+EOF
+echo "ArchWay Linux Installed! reboot in 5 seconds" 
+umount -R /mnt 
+sleep 5 
+reboot
