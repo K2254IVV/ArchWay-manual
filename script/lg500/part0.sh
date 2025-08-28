c68cc201b1841c2d6d5aa9de113e87da0410555b
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-08-28 08:13:30 +0300

    Update part0.sh

--- script/lg500/part0.sh
+++ script/lg500/part0.sh
@@ -13,6 +13,9 @@ read -p "Enter your timezone (example Europe/Moscow): " timezone
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime # Adjust to your timezone
 hwclock --systohc --utc
 echo ArchWayBTW > /etc/hostname
+echo "Setting the locale"
+nano /etc/locale.gen
+locale-gen
 passwd
 read -p "Enter desired username: " username
 useradd -m -s /bin/bash $username
